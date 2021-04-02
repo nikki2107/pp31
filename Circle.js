b1ca278975591371a4f715ab2175e9077a0e4bfc
nikki2107 <71547789+nikki2107@users.noreply.github.com>
2021-04-02 22:29:56 +0530

    Add files via upload

---
+++ Circle.js
@@ -0,0 +1,30 @@
+class Circle{
+    constructor(x,y,radius){
+    var options = {
+       
+        restitution : 0.1,
+        friction : 1,
+        density : 1.2
+    }
+
+        this.body = Bodies.circle(x,y,radius,options)
+        this.x = x
+        this.y = y
+        this.radius = radius
+        
+       
+        World.add(world,this.body)
+
+    }
+
+    display(){
+
+        var pos = this.body.position
+        push()
+        translate(pos.x,pos.y)
+        rectMode(CENTER)
+        fill("white")
+        ellipse(0,0,10,10)
+        pop();
+        }
+        }
